7943870523802eac43dde8e6e48e335fedf130f6
Mato Juricic <mato.juricic@digacon-software.com>
2022-12-03 15:05:29 +0100

    Implement tab switching logic

--- src/components/ViewSelector/ViewSelector.tsx
+++ src/components/ViewSelector/ViewSelector.tsx
@@ -3,19 +3,34 @@ import Tabs from "@mui/material/Tabs";
 import TableRowsRoundedIcon from "@mui/icons-material/TableRowsRounded";
 import EqualizerRoundedIcon from "@mui/icons-material/EqualizerRounded";
 import "./ViewSelector.css";
+import { useRecoilState } from "recoil";
+import { selectedPageState } from "../../store/atoms";
 
 const ViewSelector = () => {
+  const [selectedPage, setSelectedPage] = useRecoilState(selectedPageState);
+
+  const handleChangePage = (
+    event: React.SyntheticEvent,
+    newSelectedPage: string,
+  ) => {
+    setSelectedPage(newSelectedPage);
+  };
+
   return (
     <>
       <div className="vertical-tabs-container">
-        <Tabs value={"tabular"} orientation="vertical">
+        <Tabs
+          value={selectedPage}
+          orientation="vertical"
+          onChange={handleChangePage}
+        >
           <Tab
-            value="tabular"
+            value="mavg"
             icon={<TableRowsRoundedIcon />}
             className="vertical-tab"
           />
           <Tab
-            value="visual"
+            value="annualavg"
             icon={<EqualizerRoundedIcon />}
             className="vertical-tab"
           />
