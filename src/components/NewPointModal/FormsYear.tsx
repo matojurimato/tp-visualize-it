5b308a4b33765eef616451e90c25a0c4c07d1ac2
Mato Juricic <mato.juricic@digacon-software.com>
2022-12-09 13:31:02 +0100

    Implement form validation for annual entries
    Values entered in the NewPointModal while in ChartView are now being validated.
    The GCM name cannot be an empty string, and the annual value must be a valid
    number. Additionally, if the chart is showing precipitation values, the
    entered value cannot be a negative number.

--- src/components/NewPointModal/FormsYear.tsx
+++ src/components/NewPointModal/FormsYear.tsx
@@ -2,29 +2,74 @@ import { Button } from "@mui/material";
 import TextField from "@mui/material/TextField";
 import Box from "@mui/system/Box";
 import { useState } from "react";
+import { useRecoilValue } from "recoil";
+import { selectedTypeState } from "../../store/atoms";
 import "./NewPointModal.css";
 
 const FormsYear: React.FC<{
   handleSubmitButton: (gcmName: string, yearValue: number[]) => void;
 }> = (props) => {
-  const [gcmName, setGcmName] = useState<string>("");
-  const [yearValue, setYearValue] = useState(0);
+  const [gcmNameValue, setGcmName] = useState<string>("");
+  const [gcmNameError, setGcmError] = useState(false);
+  const [yearValue, setYearValue] = useState("");
+  const [yearError, setYearError] = useState(false);
+  const [yearErrorMessage, setYearErrorMessage] = useState("");
 
-  const changeGcmNameHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const selectedType = useRecoilValue(selectedTypeState);
+
+  const submitButtonDisabled = gcmNameError || yearError;
+
+  const gcmNameOnChangeHandler = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
     event.preventDefault();
     setGcmName(event.target.value);
   };
 
-  const changeYearValueHandler = (
+  const gcmNameOnBlurHandler = (event: React.FocusEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    if (event.target.value.length) {
+      setGcmError(false);
+      setGcmName(event.target.value);
+    } else {
+      setGcmError(true);
+    }
+  };
+
+  const yearValueOnChangeHandler = (
     event: React.ChangeEvent<HTMLInputElement>,
   ) => {
     event.preventDefault();
-    setYearValue(parseFloat(event.target.value));
+    setYearValue(event.target.value);
+  };
+
+  const yearValueOnBlurHandler = (
+    event: React.FocusEvent<HTMLInputElement>,
+  ) => {
+    event.preventDefault();
+    if (event.target.value.length === 0) {
+      setYearError(true);
+      setYearErrorMessage("A value is required");
+    } else if (
+      selectedType.apiAbbreviation === "pr" &&
+      parseFloat(event.target.value) < 0
+    ) {
+      setYearError(true);
+      setYearErrorMessage("Precipitation values can't be negative");
+    } else if (isNaN(parseFloat(event.target.value))) {
+      setYearValue("");
+      setYearError(true);
+      setYearErrorMessage("This value must be a number");
+    } else {
+      setYearError(false);
+      setYearErrorMessage("");
+      setYearValue(event.target.value);
+    }
   };
 
   const submitHandler = (event: React.MouseEvent<HTMLElement>) => {
     event.preventDefault();
-    props.handleSubmitButton(gcmName, [yearValue]);
+    props.handleSubmitButton(gcmNameValue, [parseFloat(yearValue)]);
   };
 
   return (
@@ -36,23 +81,32 @@ const FormsYear: React.FC<{
             variant="outlined"
             type="text"
             label="GCM name"
-            value={gcmName}
-            onChange={changeGcmNameHandler}
+            value={gcmNameValue}
+            onChange={gcmNameOnChangeHandler}
+            onBlur={gcmNameOnBlurHandler}
+            error={gcmNameError}
+            helperText={gcmNameError && "A GCM name is required"}
             required
           />
         </div>
         <TextField
           className="input-field"
           variant="outlined"
-          type="number"
           label="Year value"
           value={yearValue}
-          onChange={changeYearValueHandler}
+          onChange={yearValueOnChangeHandler}
+          onBlur={yearValueOnBlurHandler}
+          error={yearError}
+          helperText={yearErrorMessage}
           required
         />
         <div className="button-container">
           <div className="submit-button">
-            <Button onClick={submitHandler} variant="contained">
+            <Button
+              onClick={submitHandler}
+              variant="contained"
+              disabled={submitButtonDisabled}
+            >
               ADD NEW POINT
             </Button>
           </div>
